b05bfb44b8e25abe184fbf66179983ed227a3b30
LifeHckr <jarodthereal@gmail.com>
2025-07-17 10:16:53 -0700

    Finished Lick The Slug
    Edited settings to better respect pixel art
    Added Font, still not default
    Need glue between scenes

---
+++ LickTheSlug/LickTheSlug.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+
+public partial class LickTheSlug : Node2D
+{
+    [Export]
+    private Slug _slug;
+    [Export] private Sprite2D _tongue;
+    private float _tongueSpeed = 100;
+    [Export] private AudioStreamPlayer _lickSound;
+    [Export]
+    private Label _timeLeftLabel;
+    [Export]
+    private Label _scoreLabel;
+    private int _score;
+    [Export]
+    private Timer _timer;
+
+    public override void _EnterTree()
+    {
+        _slug.Licked += SlugLicked;
+        _timer.Timeout += TimesUp;
+    }
+
+    public override void _Process(double delta)
+    {
+        _timeLeftLabel.Text = "00:" + _timer.TimeLeft.ToString("00");
+        Vector2 tongueDirection = Input.GetVector("Left", "Right", "Up", "Down");
+        _tongue.Position += tongueDirection * _tongueSpeed * (float)delta;
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event.IsActionPressed("ButtonA") && _tongue.Position.DistanceSquaredTo(_slug.Position) < _slug.ValidClickRadiusSquared)
+        {
+            _slug.GotLicked();
+        }
+    }
+
+    private void SlugLicked()
+    {
+        IncScore();
+    }
+
+    private void IncScore()
+    {
+        _score += 1;
+        _scoreLabel.Text = "Score: " + _score;
+        _lickSound.Play();
+    }
+
+    private void TimesUp()
+    {
+        ProcessMode = ProcessModeEnum.Disabled;
+        GD.Print("Time's up, pausing scene for now!");
+    }
+}
---
+++ LickTheSlug/Scripts/Slug.cs
@@ -0,0 +1,50 @@
+using Godot;
+using System;
+
+public partial class Slug : Sprite2D
+{
+    private float _speed = 20;
+    private Vector2 _direction = Vector2.Zero;
+    
+    private readonly Vector2[] _directions = { Vector2.Left, Vector2.Right, Vector2.Down, Vector2.Up };
+    
+    public override void _EnterTree()
+    {
+        Texture2D tongue = GD.Load<Texture2D>("res://LickTheSlug/Tongue.png");
+        GD.Randomize();
+        Position = new Vector2(GD.RandRange(100, 860), GD.RandRange(100, 440));
+        _direction = _directions[GD.RandRange(0, _directions.Length-1)];
+        Rotation = _direction.Angle();
+        Input.SetCustomMouseCursor(tongue);
+    }
+
+    public override void _ExitTree()
+    {
+        Input.SetCustomMouseCursor(null);
+    }
+
+    public override void _Process(double delta)
+    {
+        Position += _direction * _speed * (float)delta;
+    }
+
+    public float ValidClickRadiusSquared = 1000;
+
+    public delegate void LickedHandler();
+    public event LickedHandler Licked;
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseButton mouseButton && mouseButton.IsActionPressed("Click") && GetGlobalMousePosition().DistanceSquaredTo(GlobalPosition) <= ValidClickRadiusSquared)
+        {
+            GotLicked();
+        }
+    }
+
+    public void GotLicked()
+    {
+        Licked?.Invoke();
+        _direction = _directions[GD.RandRange(0, _directions.Length-1)];
+        Rotation = _direction.Angle();
+    }
+}
